f5e77fafd64fffcd13341ba39ec10a9ec9b1636e
gustavo_lourenco <gustavo_lourenco@10.1.49.218>
2013-08-12 17:01:47 -0300

    Criada classe Validadores, mas não esta sendo utilizada

---
+++ src/br/senai/sc/model/negocio/Validadores.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**
+ * @version 1.0 12/08/2013
+ * @author gustavo_lourenco
+ */
+public class Validadores {
+
+    public static void somenteLetras() {
+    }
+
+    public static void somenteNumeros() {
+    }
+}
